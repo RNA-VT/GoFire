f9bacccb25de3e82e49f7b89ea66cb5e99aa55c6
Tushar Shah <twoshark@gmail.com>
2020-01-20 11:28:06 -0500

    use uid for key in components return map, remove extra space in rpi pin map

--- src/app/routes.go
+++ src/app/routes.go
@@ -71,7 +71,7 @@ func (a *Application) getComponents(c echo.Context) error {
 	components := make(map[string]component.Solenoid, count)
 	for i := 0; i < len(a.Cluster.SlaveDevices); i++ {
 		for j := 0; j < len(a.Cluster.SlaveDevices[i].Solenoids); j++ {
-			components[a.Cluster.SlaveDevices[i].Solenoids[j].Name] = a.Cluster.SlaveDevices[i].Solenoids[j]
+			components[a.Cluster.SlaveDevices[i].Solenoids[j].UID] = a.Cluster.SlaveDevices[i].Solenoids[j]
 		}
 	}
 	container["components"] = components
