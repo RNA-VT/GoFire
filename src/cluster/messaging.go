fc23c4bd813710767dace486af3e5a557402234e
Tushar Shah <twoshark@gmail.com>
2020-02-03 23:52:46 -0500

    add methods to post to warn and panic the cluster

--- src/cluster/messaging.go
+++ src/cluster/messaging.go
@@ -1,6 +1,9 @@
 package cluster
 
-import mc "firecontroller/microcontroller"
+import (
+	"firecontroller/microcontroller"
+	mc "firecontroller/microcontroller"
+)
 
 //JoinNetworkMessage is the registration request
 type JoinNetworkMessage struct {
@@ -11,4 +14,57 @@ type JoinNetworkMessage struct {
 type PeerUpdateMessage struct {
 	Source  mc.Microcontroller
 	Cluster Cluster
+}
+
+//PeerErrorMessage -
+type PeerErrorMessage struct {
+	Panic bool
+	PeerInfoMessage
+}
+
+//PeerInfoMessage -
+type PeerInfoMessage struct {
+	Source   mc.Microcontroller
+	Messages []string
+}
+
+//ClusterError - Log the errors, warn the others and then panic.
+func (c *Cluster) ClusterError(panicAfterWarning bool, panicCluster bool, notGoodThings ...string) {
+	//Errors that render this microcontroller unusable, but do not effect the rest of the cluster
+	if panicCluster {
+		c.EverybodyPanic(notGoodThings...)
+	} else {
+		c.WarnTheOthers(notGoodThings...)
+	}
+	if panicAfterWarning {
+		panic(notGoodThings)
+	}
+}
+
+//WarnTheOthers - POST Error(s) to cluster.
+func (c *Cluster) WarnTheOthers(msgs ...string) {
+	//This path should be used for errors that make this instance of GoFire unavailable
+	message := PeerErrorMessage{
+		Panic: false,
+	}
+	message.Source = c.Me
+	message.Messages = msgs
+	c.tellTheOthers("/errors/warn", message)
+}
+
+//EverybodyPanic - Meant for Errors that should stop the entire cluster
+func (c *Cluster) EverybodyPanic(notGoodThings ...string) {
+	message := PeerErrorMessage{
+		Panic: true,
+	}
+	message.Source = c.Me
+	message.Messages = notGoodThings
+	c.tellTheOthers("errors/panic", message)
+}
+
+func (c *Cluster) tellTheOthers(path string, msg interface{}) {
+	c.UpdatePeers(
+		path,
+		msg,
+		[]microcontroller.Microcontroller{c.Me})
 }
